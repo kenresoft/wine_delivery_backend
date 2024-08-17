8946bcc09f0269ec9e0766c90d3b71e241e16608
kenresoft <amadikenneth8@gmail.com>
2024-08-17 19:50:48 +0100

    Modified models/Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,21 +1,14 @@
 const mongoose = require('mongoose');
-
-const reviewSchema = new mongoose.Schema({
-    userId: { type: String, required: true },
-    // userid: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    review: { type: String, required: true }
-});
+const review = require('./Review');
 
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    // category: { type: String, required: true },
-    category: { type: mongoose.Schema.Types.String, ref: 'Category', required: true },
+    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
     image: { type: String, required: true },
     price: { type: Number, required: true },
-    rating: { type: Number, required: true },
     alcoholContent: { type: Number, required: true },
     description: { type: String, required: true },
-    reviews: [reviewSchema]
+    reviews: [review.schema],
 });
 
 const Product = mongoose.model('Product', productSchema);
