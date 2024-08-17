cf6dc86518971b54a41960284cc6491128b3c60b
kenresoft <amadikenneth8@gmail.com>
2024-08-17 19:50:30 +0100

    Modified models/Review.js

--- models/Review.js
+++ models/Review.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema({
-    // productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    rating: { type: Number, required: true },
     review: { type: String, required: true }
 });
 
