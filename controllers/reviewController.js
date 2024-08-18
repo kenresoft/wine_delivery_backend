458c2065e360c4d227ad1202043fde1a2f7a8b99
kenresoft <amadikenneth8@gmail.com>
2024-08-18 13:57:42 +0100

    Modified controllers/reviewController.js

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,22 +1,29 @@
 const Review = require('../models/Review');
 const Product = require('../models/Product');
+const User = require('../models/User');
 
 exports.createReview = async (req, res) => {
     try {
         const { productId, rating, review } = req.body;
+        const user = await User.findById(req.user.id, { password: 0, isAdmin: 0 });  // exclude password
+
         const userReview = await Review.create({
-            userId: req.user.id,
+            user,
             rating,
             review,
         });
 
         // Update product rating
         const product = await Product.findById(productId);
-        product.reviews.push(userReview);
-        
-        await product.save();
+        if (!product) {
+            res.status(404).json({ success: false, message: 'Product not found' });
+        }
+        else {
+            product.reviews.push(userReview);
+            await product.save();
+            res.status(201).json({ success: true, review: userReview });
+        }
 
-        res.status(201).json({ success: true, review: userReview });
     } catch (error) {
         res.status(400).json({ success: false, error: error.message });
     }
@@ -24,8 +31,13 @@ exports.createReview = async (req, res) => {
 
 exports.getProductReviews = async (req, res) => {
     try {
-        const reviews = await Review.find({ productId: req.params.productId }).populate('userId', 'name');
-        res.status(200).json({ success: true, reviews });
+        const product = await Product.findById(req.params.productId).populate('reviews.user', { password: 0, isAdmin: 0 });
+        if (!product) {
+            res.status(404).json({ success: false, message: 'Product not found' });
+        }
+        else {
+            res.status(200).json({ success: true, reviews: product.reviews });
+        }
     } catch (error) {
         res.status(400).json({ success: false, error: error.message });
     }
