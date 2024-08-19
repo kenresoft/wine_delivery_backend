1774359d1250864cc0eb73c9147e90c544b3dd7e
kenresoft <amadikenneth8@gmail.com>
2024-08-19 15:22:00 +0100

    Added models/Favorite.js

---
+++ models/Favorite.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const favoriteSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+});
+
+const Favorite = mongoose.model('Favorite', favoriteSchema);
+
+module.exports = Favorite;
