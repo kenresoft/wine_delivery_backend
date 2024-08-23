227242fdafb59d5ed9fde1211259819131166356
kenresoft <amadikenneth8@gmail.com>
2024-08-23 21:08:20 +0100

    Modified routes/userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const { getUsers, deleteUser, getUserById, updateUser, getUsersFavorites, getUserFavoritesById } = require('../controllers/userController');
+const { getUsers, deleteUser, getUserById, updateUser } = require('../controllers/userController');
 const { protect, admin } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.get('/', protect, admin, getUsers);
-router.get('/favorites/', protect, admin, getUsersFavorites);
 router.delete('/:id', protect, admin, deleteUser);
 router.get('/:id', protect, admin, getUserById);
-router.get('/favorites/:productId', protect, admin, getUserFavoritesById);
 router.put('/:id', protect, admin, updateUser);
-
 module.exports = router;
