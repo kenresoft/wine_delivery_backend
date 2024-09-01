98b321e905fb5d03a00665af0b9e6d114830a107
kenresoft <amadikenneth8@gmail.com>
2024-09-01 13:07:53 +0100

    Disabled "npm test" on workflow

--- user.test.js
+++ user.test.js
@@ -1,9 +1,10 @@
 // index.js
+const { test } = require('jest');
 function add(a, b) {
     return a + b;
-  }
-  
-  // user.test.js
-  test('adds numbers correctly', () => {
+}
+
+// user.test.js
+test('adds numbers correctly', () => {
     expect(add(2, 3)).toBe(5);
-  });
+});
