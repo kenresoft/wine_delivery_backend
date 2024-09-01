83b8188e190782f0e92846fff5f45c4a69d4d650
kenresoft <amadikenneth8@gmail.com>
2024-09-01 13:13:35 +0100

    Modified controllers/orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,7 +1,13 @@
 const Order = require('../models/Order');
 const Product = require('../models/Product');
+const Shipment = require('../models/Shipment');
+const User = require('../models/User');
+const Cart = require('../models/Cart');
+const d = require('./cartController');
+require('dotenv').config();
+const Stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-exports.createOrder = async (req, res) => {
+/* exports.createOrder = async (req, res) => {
     try {
         const { items, shippingAddress, paymentMethod, totalPrice } = req.body;
 
@@ -32,11 +38,103 @@ exports.createOrder = async (req, res) => {
     } catch (error) {
         res.status(400).json({ success: false, error: error.message });
     }
+}; */
+
+exports.createOrder = async (req, res) => {
+    try {
+        const { subTotal, description, currency } = req.body;
+        const user = await User.findById(req.user.id, { password: 0, isAdmin: 0 });
+
+        if (!user) {
+            return res.status(401).json({ message: 'Unauthorized access' });
+        }
+
+        const cart = await Cart.findOne({ user: user._id });
+
+        if (!cart) {
+            return res.status(400).json({ message: 'No items in the cart for you to order' });
+        }
+
+        // Retrieve the user's default or preferred shipping address
+        const shipment = await Shipment.findOne({ user: user });
+
+        if (!shipment) {
+            return res.status(400).json({ message: 'No shipping address found for this user' });
+        }
+
+        // Create order document
+        const order = new Order({
+            user: user._id, // Only store user ID
+            items: cart.items,
+            shipment: shipment._id, // Only store shipment ID
+            subTotal: subTotal,
+            totalCost: subTotal + shipment.deliveryCost,
+        });
+
+        // const customerId = user ? user._id.toString() : null;
+
+        const paymentResponse = await processPayment(
+            subTotal,
+            description,
+            currency,
+        );
+
+        if (!paymentResponse.success) {
+            return res.status(400).json({
+                success: paymentResponse.success,
+                message: paymentResponse.message,
+            });
+        }
+
+        order.paymentDetails = paymentResponse.data; // Store payment details in order
+
+        // Save the order
+        await order.save();
+
+        // Reduce the stock count for each product
+        /* for (const item of cart) {
+            await Product.findByIdAndUpdate(item.productId, { $inc: { countInStock: -item.quantity } });
+        } */
+
+        res.status(201).json({ message: 'Order created successfully', order });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error: ' + error });
+    }
 };
+
+async function processPayment(amount, description, currency) {
+    try {
+        const paymentIntent = await Stripe.paymentIntents.create({
+            amount: amount * 100,
+            currency: currency || 'usd',
+            description,
+            automatic_payment_methods: {
+                enabled: true,
+            },
+        });
+
+        return {
+            success: true,
+            message: 'Payment processed successfully:' + paymentIntent,
+            data: {
+                paymentIntent: paymentIntent.client_secret,
+                publishableKey: process.env.PUBLISHABLE_KEY
+            },
+        };
+    } catch (error) {
+        console.error('Payment error:', error);
+        return {
+            success: false,
+            message: 'Payment failed',
+            error: error.message,
+        };
+    }
+}
 
 exports.getUserOrders = async (req, res) => {
     try {
-        const orders = await Order.find({ userId: req.user.id }).populate('items.productId');
+        const orders = await Order.find({ user: req.user.id })/* .populate('items.product') */;
         res.status(200).json({ success: true, orders });
     } catch (error) {
         res.status(400).json({ success: false, error: error.message });
@@ -45,7 +143,7 @@ exports.getUserOrders = async (req, res) => {
 
 exports.getOrderById = async (req, res) => {
     try {
-        const order = await Order.findById(req.params.id).populate('items.productId');
+        const order = await Order.findById(req.params.id)/* .populate('items.product') */;
         if (!order) {
             return res.status(404).json({ success: false, message: 'Order not found' });
         }
