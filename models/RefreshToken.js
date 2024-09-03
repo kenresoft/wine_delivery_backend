cc76c0671ded1230a6eda815a1f5d3ac600df6b2
kenresoft <amadikenneth8@gmail.com>
2024-09-03 21:57:46 +0100

    Added models/RefreshToken.js

---
+++ models/RefreshToken.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const RefreshTokenSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  token: { type: String, required: true },
+  expiresAt: { type: Date, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('RefreshToken', RefreshTokenSchema);
