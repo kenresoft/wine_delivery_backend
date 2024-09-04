47563978885520c3e6224c3770d62fc627c7d216
kenresoft <amadikenneth8@gmail.com>
2024-09-04 22:18:25 +0100

    Modified routes/couponRoutes.js and routes/favoriteRoutes.js

--- routes/couponRoutes.js
+++ routes/couponRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { protect, admin } = require('../middleware/authMiddleware');
+const { isAuthenticated: protect, admin } = require('../middleware/authMiddleware');
 const { createCoupon, getAllCoupons, getCouponByCode, updateCoupon, deleteCoupon } = require('../controllers/couponController');
 
 router.post('/', protect, admin, createCoupon);
