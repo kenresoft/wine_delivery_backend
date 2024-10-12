a8c15c46a3efc1f262661fd38f8889b994884a74
kenresoft <amadikenneth8@gmail.com>
2024-10-12 11:59:52 +0100

    Update models/Category.js

--- models/Category.js
+++ models/Category.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true }
+    name: { type: String, required: true, unique: true },
+    image: { type: String },
+    description: { type: String },
 });
 
 const Category = mongoose.model('Category', categorySchema);
