adad01840fbd2294413e84bd3f2295c0628484ef
kenresoft <amadikenneth8@gmail.com>
2024-10-17 23:47:18 +0100

    Modified routes/productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,25 @@
+// routes/productRoutes.js
 const express = require('express');
-const { createProduct, getAllProducts, getProductById, updateProduct, deleteProduct, getProductsByIds } = require('../controllers/productController');
+const {
+    createProduct,
+    getAllProducts,
+    getProductById,
+    updateProduct,
+    deleteProduct,
+    getProductsByIds,
+    addReview
+} = require('../controllers/productController');
+
 const { isAuthenticated: protect, admin } = require('../middleware/authMiddleware');
 const router = express.Router();
 
-router.post('/', protect, admin, createProduct);
-router.get('/', getAllProducts);
-router.get('/ids/:ids', getProductsByIds);
-router.get('/:id', getProductById);
-router.put('/:id', protect, admin, updateProduct);
-router.delete('/:id', protect, admin, deleteProduct);
+router.post('/', protect, admin, createProduct);           // Create product
+router.get('/', getAllProducts);                           // Get all products
+router.get('/ids/:ids', getProductsByIds);                 // Get products by multiple IDs
+router.get('/:id', getProductById);                        // Get single product by ID
+router.put('/:id', protect, admin, updateProduct);         // Update product by ID
+router.delete('/:id', protect, admin, deleteProduct);      // Delete product by ID
+
+router.post('/review/:id', protect, admin, addReview); // Add review to product
 
 module.exports = router;
