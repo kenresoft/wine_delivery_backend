c8a9eed4e1559538241b467c4d9862a1dea67785
kenresoft <amadikenneth8@gmail.com>
2024-10-17 23:46:51 +0100

    Modified models/Review.js

--- models/Review.js
+++ models/Review.js
@@ -1,10 +1,13 @@
+// models/Review.js
 const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    rating: { type: Number, required: true },
-    review: { type: String, required: true }
+    rating: { type: Number, required: true, min: 1, max: 5 },
+    comment: { type: String, required: true },
+}, {
+    timestamps: true
 });
 
 const Review = mongoose.model('Review', reviewSchema);
-module.exports = Review;
+module.exports = { Review, reviewSchema };
