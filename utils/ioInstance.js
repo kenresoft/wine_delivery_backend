a8889b6088db4aaa5c9d0fcb21e62a15a8958eee
kenresoft <amadikenneth8@gmail.com>
2024-10-18 23:53:21 +0100

    Updated utils/ioInstance.js

---
+++ utils/ioInstance.js
@@ -0,0 +1,34 @@
+const { isSocketAuthenticated } = require('../middleware/authMiddleware');
+let io;
+
+module.exports = {
+    init: (server) => {
+        io = require('socket.io')(server, {
+            cors: {
+                origin: "*",
+                methods: ["GET", "POST"]
+            }
+        });
+        io.use(isSocketAuthenticated);
+
+        io.on('connection', (socket) => {
+            console.log(`User connected: ${socket.user.email}`);
+
+            socket.on('sendMessage', (message) => {
+                console.log(`Message from ${socket.user.email}: ${message}`);
+                io.emit('receiveMessage', `${socket.user.email}: ${message}`);
+            });
+
+            socket.on('disconnect', () => {
+                console.log(`User disconnected: ${socket.user.email}`);
+            });
+        });
+        return io;
+    },
+    getIO: () => {
+        if (!io) {
+            throw new Error("Socket.io not initialized");
+        }
+        return io;
+    }
+};
