8be42543ebac13db279004679c4e0438a66c12a9
kenresoft <amadikenneth8@gmail.com>
2024-11-05 03:35:22 +0100

    Update .gitignore and config/firebaseAdmin.js

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = require('./firebase-adminsdk-serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
