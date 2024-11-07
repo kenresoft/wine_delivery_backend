b3f90c0abfaddaba706f82df5cee483454d7c801
kenresoft <amadikenneth8@gmail.com>
2024-11-07 15:48:03 +0100

    Modified controllers/notificationController.js

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -3,7 +3,7 @@ const Notification = require('../models/Notification');
 exports.createNotification = async (req, res) => {
     try {
         const notification = await Notification.create({
-            userId: req.user.id,
+            user: req.user.id,
             ...req.body,
         });
         res.status(201).json({ success: true, notification });
@@ -14,7 +14,7 @@ exports.createNotification = async (req, res) => {
 
 exports.getUserNotifications = async (req, res) => {
     try {
-        const notifications = await Notification.find({ userId: req.user.id });
+        const notifications = await Notification.find({ user: req.user.id }).sort({ createdAt: -1 });
         res.status(200).json({ success: true, notifications });
     } catch (error) {
         res.status(400).json({ success: false, error: error.message });
